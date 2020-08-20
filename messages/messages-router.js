7e3c0e29e27ee12da2efd00d32d117354fd1b751
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-08-20 19:23:23 -0400

    Saving changes to Guided Project

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,26 +1,84 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
+    // translates to `SELECT * FROM messages;`
+    const messages = await db.select("*").from("messages");
 
-})
+    res.json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
+  // translates to `SELECT * FROM messages WHERE id = ?;`
+  try {
+    const [message] = await db
+      .select("*")
+      .from("messages")
+      .where("id", req.params.id)
+      .limit(1);
 
-})
+    res.json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  // translates to `INSERT TO messages (title, contents) VALUES (?, ?);`
+  try {
+    const [id] = await db
+      .insert({
+        // database will automatically generate the ID
+        title: req.body.title,
+        contents: req.body.contents,
+      })
+      .into("messages");
 
-})
+    // translates to `SELECT * FROM messages WHERE id = ? LIMIT 1;`
+    const message = await db("messages").where("id", id).first();
 
-router.put("/:id", (req, res, next) => {
+    res.status(201).json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.put("/:id", async (req, res, next) => {
+  try {
+    // translates to `UPDATE messages SET title = ? AND contents = ? WHERE id = ?;`
+    await db("messages")
+      .update({
+        title: req.body.title,
+        contents: req.body.contents,
+      })
+      .where("id", req.params.id);
 
-router.delete("/:id", (req, res, next) => {
+    // translates to `SELECT * FROM messages WHERE id = ? LIMIT 1;`
+    const message = await db("messages").where("id", req.params.id).first();
 
-})
+    res.json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.delete("/:id", async (req, res, next) => {
+  try {
+    // translates to `DELETE FROM messages WHERE id = ?;`
+    await db("messages").where("id", req.params.id).del();
+
+    // since we no longer have a resource to return, just send a 204.
+    // which means "success, but no response data is being sent".
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
